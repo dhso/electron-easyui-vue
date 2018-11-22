83a28305bc7a3bf1d81d078bbafaae600e5efd7c
hao dong <dhso.hadong@gmail.com>
2018-11-22 22:40:42 +0800

    update

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-import App from './App'
-import router from './router'
-import store from './store'
-
 import 'vx-easyui/dist/themes/bootstrap/easyui.css'
 import 'vx-easyui/dist/themes/icon.css'
 import 'vx-easyui/dist/themes/vue.css'
 import EasyUI from 'vx-easyui'
+
+import App from './App'
+import router from './router'
+import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
